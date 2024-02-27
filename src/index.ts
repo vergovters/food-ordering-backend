808c3f680d9b51678ed2e7fa636471ff1b396ba8
Illia Fialkivskiji <fialkivskiji@gmail.com>
2024-02-27 19:06:18 +0200

    connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, {Request, Response} from "express"
 import cors from "cors"
 import "dotenv/config"
+import mongoose from "mongoose";
+
+mongoose
+    .connect(process.env.MONGO_DB_CONNECTION as string)
+    .then(()=>console.log("connected to db"))
 
 const app = express();
 app.use(express.json());
