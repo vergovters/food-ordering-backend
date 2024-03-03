bf719068ef8c0c9f808988b740d832ad6be33dc9
illia <fialkivskiji@gmail.com>
2024-03-03 17:05:26 +0200

    create user api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express"
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose";
+import UserRouter from "./routes/UserRoutes"
+
 
 mongoose
     .connect(process.env.MONGO_DB_CONNECTION as string)
@@ -11,11 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test",async (req: Request, res: Response) => {
-    res.json({
-        message: "hello"
-    })
-}); 
+app.use("/api/user", UserRouter)
 
 app.listen(7000, ()=>{
     console.log("server started on 7000")
