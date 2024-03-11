707e659f3d398b7de6530ea6af72489977cec6af
illia <fialkivskiji@gmail.com>
2024-03-11 23:32:40 +0200

    m

---
+++ src/middleware/validations.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  };
+
+export const validateUserReq = [
+    body("name").isString().notEmpty().withMessage("Name must be a string"),
+    body("adressLine1").isString().notEmpty().withMessage("Address must be a string"),
+    body("city").isString().notEmpty().withMessage("City must be a string"),
+    body("country").isString().notEmpty().withMessage("Country must be a string"),
+    handleValidationErrors,
+]
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,10 +1,12 @@
+import { validateUserReq } from './../middleware/validations';
+
 import express from "express";
 import UserController from "../controllers/UserController";
-import { jwtCheck, jwtParse } from "../middleware/Auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router()
 
 router.post("/",jwtCheck, UserController.createCurrentUser)
-router.put("/", jwtCheck, jwtParse ,UserController.updateCurrentUser)
+router.put("/", jwtCheck, validateUserReq, jwtParse ,UserController.updateCurrentUser)
 
 export default router;
