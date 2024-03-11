bcf08774776035792fba12fe831c54247ec861d3
illia <fialkivskiji@gmail.com>
2024-03-11 22:47:49 +0200

    jwtParser

--- src/models/user.ts
+++ src/models/user.ts
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
     },
-    adressLine1: {
+    addressLine1: {
         type: String,
     },
     city: {
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -5,5 +5,6 @@ import { jwtCheck } from "../middleware/Auth";
 const router = express.Router()
 
 router.post("/",jwtCheck, UserController.createCurrentUser)
+router.put("/", UserController.updateCurrentUser)
 
 export default router;
